dd88ef0ee6f0bb094ff284135d559e60d8070d14
munjungsu <91578167+munjungsu@users.noreply.github.com>
2024-11-26 19:37:18 +0900

    test

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -21,7 +21,7 @@ export default async function Page(props: {
       <div className="flex w-full items-center justify-between">
         <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
       </div>
-      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+      {/* <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
         <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
@@ -30,7 +30,7 @@ export default async function Page(props: {
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
         <Pagination totalPages={totalPages} />
-      </div>
+      </div> */}
     </div>
   );
 }
